26413f5076d5f581a8be7d66b6e9cfa1cdc38a93
Jack Steel <jacksteel@jacksteel.co.uk>
2020-11-28 15:46:13 +0000

    adds guild check attribute

---
+++ SteelBot/Attributes/GuildCheckAttribute.cs
@@ -0,0 +1,26 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SteelBot.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public sealed class GuildCheckAttribute : CheckBaseAttribute
+    {
+        private HashSet<ulong> AllowedServerIds { get; set; }
+
+        public GuildCheckAttribute(params ulong[] allowedServerIds)
+        {
+            AllowedServerIds = allowedServerIds.ToHashSet();
+        }
+
+        public override Task<bool> ExecuteCheckAsync(CommandContext ctx, bool help)
+        {
+            bool allowed = ctx.Guild != null && AllowedServerIds.Contains(ctx.Guild.Id);
+            return Task.FromResult(allowed);
+        }
+    }
+}
