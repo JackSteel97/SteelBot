a4e230f1fecc0913a0543fb9f6eabe81a317a29e
Jack Steel <jacksteel@jacksteel.co.uk>
2021-01-09 15:19:57 +0000

    tweaks stats me layout

--- SteelBot/DiscordModules/Stats/StatsDataHelper.cs
+++ SteelBot/DiscordModules/Stats/StatsDataHelper.cs
@@ -43,11 +43,12 @@ namespace SteelBot.DiscordModules.Stats
                 .WithTitle($"{username} Stats")
                 .AddField("Message Count", $"`{user.MessageCount} Messages`", true)
                 .AddField("Average Message Length", $"`{user.GetAverageMessageLength()} Characters`", true)
-                .AddField("Message Efficiency", Formatter.InlineCode(user.GetMessageEfficiency().ToString("P2")))
-                .AddField("Voice Time", $"`{user.TimeSpentInVoice.Humanize(3)}`", false)
-                //.AddField("Streaming Time", $"`{user.TimeSpentStreaming.Humanize(3)}`", false)
-                .AddField("Muted Time", $"`{user.TimeSpentMuted.Humanize(3)}`", false)
+                .AddField("Message Efficiency", Formatter.InlineCode(user.GetMessageEfficiency().ToString("P2")), true)
+                .AddField("Voice Time", $"`{user.TimeSpentInVoice.Humanize(3)}`", true)
+                .AddField("Muted Time", $"`{user.TimeSpentMuted.Humanize(3)}`", true)
+                //.AddField("\u200B", "\u200B") TODO: Uncomment when adding streaming time for 2x2 grid of times.
                 .AddField("Deafened Time", $"`{user.TimeSpentDeafened.Humanize(3)}`", false);
+                //.AddField("Streaming Time", $"`{user.TimeSpentStreaming.Humanize(3)}`", false)
             return embedBuilder;
         }
 
