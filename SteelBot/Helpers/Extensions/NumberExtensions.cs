41e289675dbce312e750317385ba3ad398988f7b
Jack Steel <jacksteel@jacksteel.co.uk>
2021-01-23 00:59:15 +0000

    increases detail on stats numbers

--- SteelBot/Helpers/Extensions/NumberExtensions.cs
+++ SteelBot/Helpers/Extensions/NumberExtensions.cs
@@ -11,18 +11,18 @@ namespace SteelBot.Helpers.Extensions
         public static string KiloFormat(this ulong value)
         {
             if (value >= 100000000000)
-                return (value / 1000000000).ToString("#,0") + " B";
+                return (value / 1000000000).ToString("##,0") + " B";
             if (value >= 10000000000)
-                return (value / 1000000000D).ToString("0.#") + " B";
+                return (value / 1000000000D).ToString("0.####") + " B";
             if (value >= 100000000)
-                return (value / 1000000).ToString("#,0") + " M";
+                return (value / 1000000).ToString("##,0") + " M";
             if (value >= 10000000)
-                return (value / 1000000D).ToString("0.#") + " M";
+                return (value / 1000000D).ToString("0.###") + " M";
             if (value >= 100000)
-                return (value / 1000).ToString("#,0") + " K";
+                return (value / 1000).ToString("##,0") + " K";
             if (value >= 10000)
-                return (value / 1000D).ToString("0.#") + " K";
-            return value.ToString("#,0");
+                return (value / 1000D).ToString("0.##") + " K";
+            return value.ToString("##,0");
         }
     }
 }
