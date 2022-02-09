516c010f42b9f39560bef26cfa5c7ad6a2b014bb
Jack Steel <jacksteel@jacksteel.co.uk>
2022-02-09 17:47:10 +0000

    adds more provider methods for pets

--- SteelBot/DataProviders/SubProviders/PetsProvider.cs
+++ SteelBot/DataProviders/SubProviders/PetsProvider.cs
@@ -59,6 +59,12 @@ namespace SteelBot.DataProviders.SubProviders
             return userHasPets;
         }
 
+        public bool TryGetPet(ulong userDiscordId, long petId, out Pet pet)
+        {
+            pet = null;
+            return PetsByUserId.TryGetValue(userDiscordId, out var pets) && pets.TryGetValue(petId, out pet);
+        }
+
         public bool BotKnowsPet(ulong userDiscordId, long petId)
         {
             return PetsByUserId.TryGetValue(userDiscordId, out var pets) && pets.ContainsKey(petId);
@@ -76,6 +82,22 @@ namespace SteelBot.DataProviders.SubProviders
             else
             {
                 pets.Add(pet.RowId, pet);
+            }
+        }
+
+        public void RemoveFromCache(Pet pet)
+        {
+            if(PetsByUserId.TryGetValue(pet.OwnerDiscordId, out var pets))
+            {
+                pets.Remove(pet.RowId);
+            }
+        }
+
+        public void UpdateInCache(Pet newPet)
+        {
+            if(PetsByUserId.TryGetValue(newPet.OwnerDiscordId, out var pets))
+            {
+                pets[newPet.RowId] = newPet;
             }
         }
 
@@ -100,6 +122,52 @@ namespace SteelBot.DataProviders.SubProviders
                 {
                     Logger.LogError("Writing Pet [{PetName}] for User [{UserId}] to the database inserted no entities. The internal cache was not changed.", pet.GetName(), pet.OwnerDiscordId);
                 }
+            }
+        }
+
+        public async Task RemovePet(ulong userDiscordId, long petId)
+        {
+            if (TryGetPet(userDiscordId, petId, out var pet))
+            {
+                Logger.LogInformation("Deleting a Pet [{PetName}] from User [{UserId}]", pet.GetName(), pet.OwnerDiscordId);
+
+                int writtenCount;
+                using(SteelBotContext db = DbContextFactory.CreateDbContext())
+                {
+                    db.Pets.Remove(pet);
+                    writtenCount=await db.SaveChangesAsync();
+                }
+
+                if(writtenCount > 0)
+                {
+                    RemoveFromCache(pet);
+                }
+                else
+                {
+                    Logger.LogError("Deleting Pet [{PetName}] from User [{UserId}] from the database altered no entities. The internal cache was not changed.", pet.GetName(), pet.OwnerDiscordId);
+                }
+            }
+        }
+
+        public async Task UpdatePet(Pet newPet)
+        {
+            int writtenCount;
+            using (SteelBotContext db = DbContextFactory.CreateDbContext())
+            {
+                // To prevent EF tracking issue, grab and alter existing value.
+                var original = db.Pets.First(u => u.RowId == newPet.RowId);
+                db.Entry(original).CurrentValues.SetValues(newPet);
+                db.Pets.Update(original);
+                writtenCount = await db.SaveChangesAsync();
+            }
+
+            if (writtenCount > 0)
+            {
+                UpdateInCache(newPet);
+            }
+            else
+            {
+                Logger.LogError("Updating Pet [{PetId}] with Owner [{UserId}] did not alter any entities. The internal cache was not changed.", newPet.RowId, newPet.OwnerDiscordId);
             }
         }
     }
