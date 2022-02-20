ce9b8c41e80c1fbd4065d0b0d4f43395771ae96d
Jack Steel <jacksteel@jacksteel.co.uk>
2022-02-20 15:05:18 +0000

    undo debug changes for chances

--- SteelBot/DiscordModules/Pets/Generation/PetFactory.cs
+++ SteelBot/DiscordModules/Pets/Generation/PetFactory.cs
@@ -111,7 +111,7 @@ namespace SteelBot.DiscordModules.Pets.Generation
         private static Rarity GetBaseRarity()
         {
             const int maxBound = 1000;
-            const double MythicalChance = 1; //0.002;
+            const double MythicalChance = 0.002;
             const double LegendaryChance = 0.02;
             const double EpicChance = 0.05;
             const double RareChance = 0.10;
