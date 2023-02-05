6f6aaf93657352a7613e92e4fc9c5a93b7f433e9
Jack Steel <jacksteel@jacksteel.co.uk>
2023-02-05 15:02:41 +0000

    :sparkles: Rework pet treats

--- SteelBot.Test/DiscordModules/Pets/Helpers/PetMathsTests.cs
+++ SteelBot.Test/DiscordModules/Pets/Helpers/PetMathsTests.cs
@@ -4,9 +4,6 @@ using Moq;
 using SteelBot.DiscordModules.Pets.Enums;
 using SteelBot.DiscordModules.Pets.Helpers;
 using SteelBot.Helpers.Levelling;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using Xunit;
 
 namespace SteelBot.Test.DiscordModules.Pets.Helpers;
@@ -14,69 +11,23 @@ namespace SteelBot.Test.DiscordModules.Pets.Helpers;
 public class PetMathsTests
 {
     [Theory]
-    [InlineData(1, Rarity.Common)]
-    [InlineData(1, Rarity.Legendary)]
-    [InlineData(1, Rarity.Mythical)]
-    [InlineData(50, Rarity.Mythical)]
-    [InlineData(60, Rarity.Mythical)]
-    [InlineData(100, Rarity.Mythical)]
-    [InlineData(150, Rarity.Mythical)]
-    public void CalculateTreatXp_ShouldBeWithinBounds(int petLevel, Rarity rarity)
+    [InlineData(1, 1, Rarity.Common)]
+    [InlineData(1, 1, Rarity.Legendary)]
+    [InlineData(1, 1, Rarity.Mythical)]
+    [InlineData(50, 40, Rarity.Mythical)]
+    [InlineData(60, 50, Rarity.Mythical)]
+    [InlineData(100, 110, Rarity.Mythical)]
+    [InlineData(150, 175, Rarity.Mythical)]
+    public void CalculateTreatXp_ShouldBeWithinBounds(int petLevel, int userLevel, Rarity rarity)
     {
         double xpRequiredForNextLevel = LevellingMaths.PetXpForLevel(petLevel + 1, rarity, false);
         double xpRequiredForThisLevel = LevellingMaths.PetXpForLevel(petLevel, rarity, false);
         double xpRequiredToLevel = xpRequiredForNextLevel - xpRequiredForThisLevel;
         var loggerMock = new Mock<ILogger>();
-        double xpGain = PetMaths.CalculateTreatXp(petLevel, rarity, 1, false, loggerMock.Object);
+        double xpGain = PetMaths.CalculateTreatXp(petLevel, rarity, 1, userLevel, loggerMock.Object);
         xpGain.Should()
-            .BeLessThan(xpRequiredToLevel)
+            .BeLessThan(xpRequiredToLevel * 1.5)
             .And
             .BeGreaterOrEqualTo(100);
-    }
-
-    [Theory]
-    [InlineData(1, Rarity.Common)]
-    [InlineData(1, Rarity.Legendary)]
-    [InlineData(1, Rarity.Mythical)]
-    [InlineData(50, Rarity.Mythical)]
-    [InlineData(60, Rarity.Mythical)]
-    [InlineData(100, Rarity.Common)]
-    [InlineData(100, Rarity.Mythical)]
-    [InlineData(150, Rarity.Mythical)]
-    [InlineData(200, Rarity.Mythical)]
-    public void CalculateTreatXp_DistributionShouldDeviateByLessThan3Percent(int petLevel, Rarity rarity)
-    {
-        double xpRequiredForNextLevel = LevellingMaths.PetXpForLevel(petLevel + 1, rarity, false);
-        double xpRequiredForThisLevel = LevellingMaths.PetXpForLevel(petLevel, rarity, false);
-        double xpRequiredToLevel = xpRequiredForNextLevel - xpRequiredForThisLevel;
-        var loggerMock = new Mock<ILogger>();
-
-        const int iterations = 1000;
-        var percentBucketCounter = new Dictionary<int, int>();
-        for (int i = 0; i < iterations; ++i)
-        {
-            double xpGain = PetMaths.CalculateTreatXp(petLevel, rarity, 1, false, loggerMock.Object);
-            double gainPercent = (xpGain / xpRequiredToLevel) * 100;
-
-            int roundedPercent = (int)Math.Round(gainPercent, MidpointRounding.AwayFromZero);
-            if (!percentBucketCounter.ContainsKey(roundedPercent))
-            {
-                percentBucketCounter.Add(roundedPercent, 1);
-            }
-            else
-            {
-                percentBucketCounter[roundedPercent] += 1;
-            }
-        }
-
-        percentBucketCounter.Count.Should().BeGreaterThan(1);
-        StandardDeviation(percentBucketCounter.Values.AsEnumerable())
-            .Should().BeLessThan(iterations * 0.03);
-    }
-    
-    private static double StandardDeviation(IEnumerable<int> values)
-    {
-        double avg = values.Average();
-        return Math.Sqrt(values.Average(v=>Math.Pow(v-avg,2)));
     }
 }
--- SteelBot/DiscordModules/Pets/Helpers/PetMaths.cs
+++ SteelBot/DiscordModules/Pets/Helpers/PetMaths.cs
@@ -8,22 +8,36 @@ namespace SteelBot.DiscordModules.Pets.Helpers;
 
 public static class PetMaths
 {
-    public static double CalculateTreatXp(int petLevel, Rarity petRarity, double petTreatXpBonus, bool isCorrupt, ILogger logger)
+    public static double CalculateTreatXp(int petLevel, Rarity petRarity, double petTreatXpBonus, int userLevel, ILogger logger)
     {
         const string formatter = "N0";
         const int lowerBound = 100;
-        double xpRequiredForNextLevel = LevellingMaths.PetXpForLevel(petLevel + 1, petRarity, isCorrupt);
-        double xpRequiredForThisLevel = LevellingMaths.PetXpForLevel(petLevel, petRarity, isCorrupt);
-        double xpRequiredToLevel = xpRequiredForNextLevel - xpRequiredForThisLevel;
-        logger.LogDebug("XP currently required to reach Level {NextLevel} from {CurrentLevel} is {XpRequiredToLevel}", petLevel + 1, petLevel, xpRequiredToLevel.ToString(formatter));
 
-        double scaledXp = xpRequiredToLevel / (1 + Math.Log(Math.Max(1, petLevel - 50), 1.5));
+        int nextUserLevel = userLevel + 1;
+        int differenceBetweenLevels = Math.Abs(userLevel - petLevel);
+
+        double xpRequiredForNextUserLevel = LevellingMaths.XpForLevel(nextUserLevel);
+        double xpRequiredForThisUserLevel = LevellingMaths.XpForLevel(userLevel);
+        double xpRequiredToLevelTheUser = xpRequiredForNextUserLevel - xpRequiredForThisUserLevel;
+        logger.LogDebug("XP currently required for user to reach Level {NextLevel} from {CurrentLevel} is {XpRequiredToLevel}", nextUserLevel, userLevel, xpRequiredToLevelTheUser.ToString(formatter));
+
+        double rarityScaler = 1 + ((int)petRarity / 10D);
+        logger.LogDebug("The rarity scaler is {RarityScaler}", rarityScaler);
+        
+        double scalingBase = (differenceBetweenLevels + 1) * rarityScaler;
+        logger.LogDebug("The scaling base is {ScalingBase}", scalingBase);
+        
+        double scalingDivider = Math.Pow(scalingBase, 0.8) / 1.5;
+        logger.LogDebug("The scaling divider is {ScalingDivider}", scalingDivider);
+        
+        double scaledXp = xpRequiredToLevelTheUser / scalingDivider;
         logger.LogDebug("After applying the scaling algorithm the max XP we can grant is {ScaledXp}", scaledXp.ToString(formatter));
 
         double upperBound = Math.Max(lowerBound + 1, Math.Round(scaledXp));
         double randomMultiplier = RandomNumberGenerator.GetInt32(101) / 100d;
         double xpGain = (randomMultiplier * (upperBound - lowerBound)) + lowerBound;
-        logger.LogDebug("The Upper Bound is {UpperBound}, the Lower Bound is {LowerBound}, the Random Co-efficient is {RandomMultiplier}", upperBound.ToString(formatter), lowerBound.ToString(formatter), randomMultiplier);
+        logger.LogDebug("The Upper Bound is {UpperBound}, the Lower Bound is {LowerBound}, the Random Co-efficient is {RandomMultiplier}", upperBound.ToString(formatter),
+            lowerBound.ToString(formatter), randomMultiplier);
 
         double finalXp = Math.Round(xpGain * petTreatXpBonus);
         logger.LogDebug("The Xp Gain calculated is {XpGain}, The Pet Treat Bonus is {PetTreatBonus}", xpGain.ToString(formatter), petTreatXpBonus);
